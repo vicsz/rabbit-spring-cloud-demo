0878823fb4bdea4df5232e439f5f526b56287d6e
VicSz <vicsz>
2018-07-06 14:48:55 -0400

    Removing EnableRabbit and Configuration Annotation that isn't needed.

--- src/main/java/com/example/rabbitspringclouddemo/RabbitSpringCloudDemoApplication.java
+++ src/main/java/com/example/rabbitspringclouddemo/RabbitSpringCloudDemoApplication.java
@@ -1,13 +1,9 @@
 package com.example.rabbitspringclouddemo;
 
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@EnableRabbit
-@Configuration
 public class RabbitSpringCloudDemoApplication {
 
 	public static void main(String[] args) {
